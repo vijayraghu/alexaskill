2bfc1601360e7a7f5d4bcd56c6300c21058536b9
vijayraghu <vijayasarathypr@gmail.com>
2018-06-08 20:26:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ from flask_ask import Ask, request, session, question, statement, audio, delegat
 from twilio.rest import Client
 
 # Declaring Twilio credentials
-account_sid = 'AC4f811e2ef41e640a2dedbb43e40ac77f'
-auth_token = 'ba2d0e0eaf43f49c34427054b7c41778'
+#account_sid = 'accountsid'
+#auth_token = 'accountauthtoken'
 client = Client(account_sid, auth_token)
 
 # Flask app should start in global layout
@@ -34,25 +34,23 @@ def Accountbalance():
 		return statement('To start using this skill, please use the companion app to authenticate on Amazon') \
 			.LinkAccount_card()
 	else:
-#delegate dialog to Alexa until all parameters are set
-    dialog_state = get_dialog_state()
-    print (dialog_state)
-    if dialog_state != "COMPLETED":
-        return delegate(speech=None)
-	  session.user.accessToken = accessToken
-    balance, accnumber, duedate  = getbalance(accessToken)
-    print (balance, accnumber, duedate)
-    #session.attributes['intent_name'] = "addchannel"
-    #session.attributes['channel_name'] = channelname
-    #speech = '<speak>' + channelname +  'can be added to your account for just ' + channelprice +  '. Do you want to go ahead and add it to your account? </speak>'
-	  message = client.messages \
+		#delegate dialog to Alexa until all parameters are set
+		dialog_state = get_dialog_state()
+		print (dialog_state)
+	
+	if dialog_state != "COMPLETED":
+		return delegate(speech=None)
+	session.user.accessToken = accessToken
+	balance, accnumber, duedate  = getbalance(accessToken)
+	print (balance, accnumber, duedate)
+	message = client.messages \
                 .create(
                      body="Your account balance is " + balance +  ". Your account number is " + accnumber + " and your bill due date is the " + duedate + ". We have sent you an SMS with the details to your mobile number.",
                      from_='+14696467609',
                      to='+9840610434'
                  )
-	  print(message.sid)
-    return statement('Your account balance is ' + balance +  '. Your account number is ' + accnumber + ' and your bill due date is the ' + duedate + '. We have sent you an SMS with the details to your mobile number.')
+	print(message.sid)
+	return statement('Your account balance is ' + balance +  '. Your account number is ' + accnumber + ' and your bill due date is the ' + duedate + '. We have sent you an SMS with the details to your mobile number.')
 	#.simple_card('Channel', speech)
 
 #Stop Intent
@@ -72,10 +70,10 @@ def session_ended():
 
 #Helper function for balance, account number and due date
 def getbalance(accessToken):
-	  balance = 'Thirteen dollars and ninety nine cents'
-	  accnumber = '34567654'
-	  duedate = '06/20/2018'
-	  return balance, accnumber, duedate
+	balance = 'Thirteen dollars and ninety nine cents',
+	accnumber = '34567654',
+	duedate = '06/20/2018'
+		return balance, accnumber, duedate
 		
 if __name__ == '__main__':
 	port = int(os.getenv('PORT', 5000))
