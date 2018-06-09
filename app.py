6365968737b90bddef2f527155cc107576d2d3b0
vijayraghu <vijayasarathypr@gmail.com>
2018-06-09 18:18:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,11 +41,9 @@ def Accountbalance():
 		balance, accnumber, duedate  = getbalance(accesstoken)
 		print(balance, accnumber, duedate)
 		client = Client(os.environ.get('TWILIO_ACCOUNT_SID'), os.environ.get('TWILIO_AUTH_TOKEN'))
-		message = client.messages.create(
-			body="Your account balance is " + balance +  ". Your account number is " + accnumber + " and your bill due date is the " + duedate + ". We have sent you an SMS with the details to your mobile number.",
-                     	from_='+14696467609',
-                     	to='+919840610434'
-		)
+		client.messages.create(from_='+14696467609', 
+				       to='+919840610434',
+				       body='Your account balance is ' + balance +  '. Your account number is ' + accnumber + ' and bill is due on ' + duedate)
 		return statement('Your account balance is ' + balance +  '. Your account number is ' + accnumber + ' and your bill due date is the ' + duedate + '. We have sent you an SMS with the details to your mobile number.')
 	#.simple_card('Channel', speech)
 
