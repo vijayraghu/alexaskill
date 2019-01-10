f618f11bafa04e3a681741aae246bdda9b809301
vijayraghu <vijayasarathypr@gmail.com>
2019-01-10 13:27:25 +0530

    Update smssurvey.py

--- smssurvey.py
+++ smssurvey.py
@@ -33,14 +33,10 @@ def requestsurvey():
 	currentsurveystatus = "RequestforSurvey"
 	text = "You have been selected as a survey participant. Kindly respond by stating Yes if you want to take the survey or No if you want to opt out'
 	# Send Request for Survey message
-	client = Client(account_sid, auth_token)
-	client.messages.create(body=text, 
-			       from_=cli,
-			       to=dnis
-			      )
+	sendSMS(dnis, text, cli)
 	conn = pymysql.connect(host=databasehost, user=databaseusername, passwd=databasepassword, port=3306, db=databasename)
 	cur = conn.cursor()
-	query = "INSERT INTO customer_survey_master("ani, current_survey_status) values (%s,%s)"
+	query = "INSERT INTO customer_survey_master(ani, current_survey_status) values (%s,%s)"
 	args = (dnis, currentsurveystatus)
 	cur.execute(query,args)
 	conn.commit()
@@ -48,33 +44,79 @@ def requestsurvey():
 	conn.close()
 	return str(response)
 
-#Proceed with survey based on response from Customer
+# Get ANI, SMS response and current survey status
 @app.route('/startsurvey', methods=['GET','POST'])
 def startsurvey():
 	callerphonenumber = request.values.get('From')
 	print (caller_phone_number)
 	input_text = request.values.get('Body')
+	current_status = ""
 	conn = pymysql.connect(host=databasehost, user=databaseusername, passwd=databasepassword, port=3306, db=databasename)
 	cur = conn.cursor()
 	cur.execute("SELECT * FROM customer_survey_master where ani='"+callerphonenumber"'")
-	current_status = ""
 	for r in curr:
-		current_status = r[0]
+		current_status = r[1]
 	cur.close()
+	cur = conn.cursor()
 	conn.close()
-	if current_status == "RequestforSurvey":
+	sendsurveyquestion(callerphonenumber, input_text, current_status)
+	return ""
+	
+# Send questions based on survey status	
+def sendsurveyquestion(phonenumber, smsresponse, currentstatus):
+	if currentstatus == "RequestforSurvey":
 		conn = pymysql.connect(host=databasehost, user=databaseusername, passwd=databasepassword, port=3306, db=databasename)
+		# Populate survey response table with response for Request for Survey
 		cur = conn.cursor()
-		cur.execute("SELECT * FROM customer_survey_master where ani='"+callerphonenumber"'")
-		# Send Request for Survey message
-		client = Client(account_sid, auth_token)
-		client.messages.create(body=text, 
-				       from_=cli, 
-				       to=callerphonenumber
-				      )
-		if input_text == "Yes":
+		query  = "INSERT INTO customer_survey_history(ani, question_key, customer_response) values (%s,%s,%s)"
+		args = (phonenumber, currentstatus, smsresponse)
+		cur.execute(query,args)
+		conn.commit()
+		cur.close()
+		# Update Survey master status to Question 1
+		cur = conn.cursor()
+		query = "UPDATE customer_survey_master set current_survey_status = %s where ani = %s"
+		args = ('Question 1', phonenumber)
+		cur.execute(query,args)
+		conn.commit()
+		curr.close()
+		# Get the next question text and call send SMS
+		cur = conn.cursor()
+		curr.execute("SELECT * from survey_question_master where question_key='"+currentstatus"'")
+		for r in curr:
+			questiontext = r[1]
+		curr.close()
+		conn.close()
+		sendSMS(phonenumber, questiontext, cli)
+		return ""
+	else:
+		conn = pymysql.connect(host=databasehost, user=databaseusername, passwd=databasepassword, port=3306, db=databasename)
+		# Get the number of questions
+		cur = conn.cursor()
+		curr.execute("SELECT count(question_key) from  survey_question_master")
+		for r in curr:
+			questioncount = int(r[0])-1
+		i=0
+		for i in questioncount
+		# Populate survey response table with response for Question
+		
 		
 	
+			    
+# Send SMS function
+def sendSMS(dnis, smsbody, cli):
+	client = Client(account_sid, auth_token)
+	client.messages.create(body=smsbody, 
+			       from_=cli,
+			       to=dnis
+			      )
+	return ""
+
+	
+	
+	
+	
+
 	question1 = "What do you think about the service you experienced. Please respond by choosing one of the options - Very Good, Good, Satisfactory, Dissatisfied'
 	question2 = "Will you recommend our service. Please respond by choosing one of the options - Definitely, Yes, No'
 	
